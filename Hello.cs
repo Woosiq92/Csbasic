69ac3ea97bd5133eddee9ede8a50b70bc5510323
Woosiq92 <uiiezl@naver.com>
2020-12-16 20:09:12 +0900

    1216

---
+++ Hello.cs
@@ -0,0 +1,12 @@
+// C:\Windows\Microsoft.NET\Framework\v4.0.30319
+
+class Hello   // class가 프로그램의 기본 단위
+			  // 필드(변수), 메소드(함수) 등의 프로그램 구성 요소들은 반드시 class 안에 존재
+{
+	
+	static void Main() // Main 프로그램의 시작점
+	{
+		System.Console.WriteLine("Hello, World"); // writeline : 화면에 문자열을 출력하는 메소드 < console 클래스 < System 이라는 네임스페이스 
+	}
+}
+
---
+++ Hello2.cs
@@ -0,0 +1,12 @@
+
+using System; // 지시문 사용 
+
+class Hello 
+{
+	
+	static void Main() 
+	{
+		Console.WriteLine("Hello, World"); // system 생략 가능 
+	}
+}
+
