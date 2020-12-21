c4d6845d133da0451a32142913818672d1e78b35
Woosiq92 <uiiezl@naver.com>
2020-12-21 20:19:39 +0900

    1221

--- CsBasic/005_string/Program.cs
+++ CsBasic/005_string/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _005_string
+namespace _005_string_Assignment 
 {
     class Program
     {
@@ -12,6 +12,10 @@ namespace _005_string
         {
             string a = "hello";
             string b = "h";
+
+            int i = 5;
+            double j = 3.14 ;
+
             b = b + "ello";
             Console.WriteLine(a == b); // True 출력 
             Console.WriteLine(" b = " + b);
@@ -19,6 +23,20 @@ namespace _005_string
             string c = b + '!' + "" + x;
             Console.WriteLine("c = " + c);
 
+            j = i;        // 암시적 형변환 : int 형은 4바이트 double 형은 8바이트기 때문에 값손실 없음 
+            i = (int)j; // 캐스트 : 더 큰 자료형을 더 작은 자료형으로 변환할 때 
+
+            bool b1 = true;
+            char c1 = 'A';
+            decimal d = 1.234m; // m은 decimal 형 의 접미사 ( 소수) 
+            float e = 1.234f; // f는 float 형의 접미사 
+            Console.WriteLine(b1); 
+            Console.WriteLine(c1);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+
+            // console.WriteLine 메소드로 여러 개의 값을 출력 
+            Console.Write("10 이하의 소수 : {0}, {1}, {2}, {3}", 2, 3, 5, 7); // 문자열에 있는 서식 지정 항목을 지정된 개체들의 문자열 표현으로 바꿉니다.
         }
     }
 }
---
+++ CsBasic/009_ConsoleFormat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009_ConsoleFormat
+{
+    class Program
+    {
+        static void Main(string[] args)  // 함수 - 외부와 소통하기 위해 ( 값을 전달하고 받고) 
+                                         // 리턴값 : 자신의 상태를 외부로 알려줌 , 함수가 종료됨 
+        {
+            int v1 = 100;
+            double v2 = 1.234; 
+            //Console.WriteLine(v1, v2) -> 두 변수를 출력하는 메소드 정의가 없기 때문에 에러 
+            // 두 변수를 출력하는 방법 
+            // 1. 두개 변수의 값을 각각 문자열로 바꾸어 연결해서 하나의 문자열로 출력하는 방법 
+
+            Console.WriteLine(v1.ToString() + ", " + v2.ToString());
+            Console.WriteLine("v1 = " + v1 + ", v2 = " + v2);
+
+            //2. 형식 정보를 사용하여 여러개의 변수나 값을 출력하는 방법 
+
+            Console.WriteLine("v1 = {0}, v2 = {1}", v1, v2);
+
+            //3. 형식문자열 앞에 $ 기호를 사용하는 문자열 보간 방법(string interpolation) 
+
+            Console.WriteLine($"v1 = {v1}, v2 = {v2}"); 
+
+        }
+    }
+}
